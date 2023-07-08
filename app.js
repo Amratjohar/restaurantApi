4a0a28840bd9f6ff9a115c10750940c6ba7a348d
Amratjohar <amratjohar@gmail.com>
2023-07-09 02:31:30 +0530

    updated

--- app.js
+++ app.js
@@ -200,10 +200,10 @@ app.delete("/deleteOrder/:id", function (req, res) {
 });
 
 //Mongodb connection
-MongoClient.connect(MONGO_URL, (err, db) => {
+MongoClient.connect(MONGO_URL, (err, client) => {
   console.log("Mongo is connected");
   if (err) console.log("Error while connecting");
-  db = db.db("Edu2023");
+  db = client.db("Edu2023");
   app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
   });
