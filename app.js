e978596c0c3898bf9af2cfcea0d6b77c38fef8ce
Amratjohar <amratjohar@gmail.com>
2023-08-27 23:29:58 +0530

    updates

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const MongoClient = mongo.MongoClient;
 // const connectionURL = "mongodb+srv://your-connection-srv-here"
 // const dbName = "your_db_name"
 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 dotenv.config();
-var cors = require("cors");
+var cors = require('cors');
 app.use(cors());
 app.use(express.json());
-let db=null;
+let db;
 
 //middleware
 
